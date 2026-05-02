f114dc7d507672d2321f437f7d21bca0f5899038
unknown <harshinigurram694@gmail.com>
2026-05-02 12:50:08 +0530

    added env to gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config/db");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-connectDB();
-
-app.use(cors());
+// Middleware
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/projects", require("./routes/projectRoutes"));
-app.use("/api/tasks", require("./routes/taskRoutes"));
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-app.listen(5000, () => console.log("🚀 Server running"));
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// PORT (Railway compatible)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
